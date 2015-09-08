f556c5ef37f9e2f98686840c709fde817a71f047
Patrick Smith <patrick@burntcaramel.com>
2015-09-08 13:03:47 +1000

    Convert services to classes

--- src/services/GoogleAnalytics.js
+++ src/services/GoogleAnalytics.js
@@ -1,29 +1,36 @@
-export function trackIdentity(userID, traits) {
+export default class GoogleAnalytics {
+  constructor(accountID) {
+    this.accountID = accountID;
+  }
 
-}
+  start() {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-export function aliasIdentity(userID, previousID) {
+    ga('create', this.accountID, 'auto');
 
-}
+    // Pages must use Analytics.Page component.
+    //ga('send', 'pageview');
+  }
 
-export function trackPage({ title }) {
-  window.ga('send', 'pageview', {
-    title
-  });
-}
+  trackIdentity(userID, traits) {
 
-export function trackEvent({ id, properties = {} }) {
-  window.ga('send', 'event', properties.category || 'All', id);
-}
 
-export function start(accountID) {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  }
 
-  ga('create', accountID, 'auto');
+  aliasIdentity(userID, previousID) {
 
-  // Pages must use Analytics.Page component.
-  //ga('send', 'pageview');
+  }
+
+  trackPage({ title }) {
+    window.ga('send', 'pageview', {
+      title
+    });
+  }
+
+  trackEvent(id, properties = {}) {
+    window.ga('send', 'event', properties.category || 'All', id);
+  }
 }
