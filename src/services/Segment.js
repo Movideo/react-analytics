a6491780882d105887dcbf7b1a615f577a39995f
Patrick Smith <patrick@burntcaramel.com>
2015-09-09 11:07:01 +1000

    Fix Segment service method name and this scope

--- src/services/Segment.js
+++ src/services/Segment.js
@@ -12,6 +12,8 @@ export default class Segment {
    * Load Segment.io API
    */
   start() {
+    const writeKey = this.writeKey;
+
     // 3.0.1 snippet
     !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1";
     analytics.load(writeKey);
@@ -25,7 +27,7 @@ export default class Segment {
    * @param  {String} title  The page’s title
    */
   trackPage({ title }) {
-    window.analytics.trackPage(title);
+    window.analytics.page(title);
   }
 
   /**
